c990f8085b42170ea532a1367cd1cd338248cd77
Frederico-dos-Santos <fredericoandrade7@gmail.com>
2022-10-17 14:39:59 -0300

    feat: class van #6

---
+++ codigo/Van.java
@@ -0,0 +1,18 @@
+package codigo;
+
+public class Van extends Veiculo{
+    @Override
+    public double calcularSeguro() {
+        return 0;
+    }
+
+    @Override
+    public double calcularIPVA() {
+        return 0;
+    }
+
+    @Override
+    public double calcularCustos() {
+        return 0;
+    }
+}
